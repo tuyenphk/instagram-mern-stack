f7bd6c16600104a5060f1ef5e2565c64037b1b42
Tuyen Pham <tuh39709@temple.edu>
2021-04-01 18:54:37 -0400

    modal for MaterialUI

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -2,25 +2,37 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 import Post from './components/Post/Post'
 import {db} from './firebase'
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import {Button} from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username:"tuyenphk",
-    //   url: "https://i.ibb.co/YNyzWWy/cover.jpg",
-    //   caption: "Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
-    // },
-    // {
-    //   username:"tuyenphk",
-    //   url: "https://i.ibb.co/Tws3HWy/Capture.png",
-    //   caption: "You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
-    // },
-    // {
-    //   username:"tuyenphk",
-    //   url: "https://i.ibb.co/SxXVr7Q/1.png",
-    //   caption: "Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
-    // }
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
   useEffect (() => {
     db.collection('posts')
@@ -31,16 +43,27 @@ function App() {
           post: doc.data()
         })));
       })
-  }, [])
+  }, []);
 
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app-header">
         <img 
             className="app-headerImage" 
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
             alt="" />
       </div>
+
+     
 
       {
         posts.map (({id, post}) => (
