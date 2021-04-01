2c501321f9cced9162cc49b977d3b26cc72b3751
Tuyen Pham <tuh39709@temple.edu>
2021-04-01 15:37:33 -0400

    render out multiple posts

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import Post from './components/Post/Post'
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username:"tuyenphk",
+      url: "https://i.ibb.co/YNyzWWy/cover.jpg",
+      caption: "Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
+    },
+    {
+      username:"tuyenphk",
+      url: "https://i.ibb.co/Tws3HWy/Capture.png",
+      caption: "You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
+    },
+    {
+      username:"tuyenphk",
+      url: "https://i.ibb.co/SxXVr7Q/1.png",
+      caption: "Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
+    }
+  ]);
+
   return (
     <div className="App">
       <div className="app-header">
@@ -11,23 +29,14 @@ function App() {
             alt="" />
       </div>
 
-      <Post
-          username="tuyenphk"
-          url="https://i.ibb.co/YNyzWWy/cover.jpg"
-          caption="Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
-      />
-
-      <Post
-          username="tuyenphk"
-          url="https://i.ibb.co/Tws3HWy/Capture.png"
-          caption="You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
-      />
-
-      <Post
-          username="tuyenphk"
-          url="https://i.ibb.co/SxXVr7Q/1.png"
-          caption="Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
-      />
+      {
+        posts.map (post => (
+          <Post username={post.username}
+                url={post.url}
+                caption={post.caption} />
+        ))
+      }
+     
     </div>
   );
 }
