bbf713c50e581c02b93e5a4d35d95e269aff0edc
Tuyen Pham <tuh39709@temple.edu>
2021-04-01 17:08:00 -0400

    useEffect to push and pull database

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -1,24 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Post from './components/Post/Post'
+import {db} from './firebase'
+
 function App() {
   const [posts, setPosts] = useState([
-    {
-      username:"tuyenphk",
-      url: "https://i.ibb.co/YNyzWWy/cover.jpg",
-      caption: "Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
-    },
-    {
-      username:"tuyenphk",
-      url: "https://i.ibb.co/Tws3HWy/Capture.png",
-      caption: "You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
-    },
-    {
-      username:"tuyenphk",
-      url: "https://i.ibb.co/SxXVr7Q/1.png",
-      caption: "Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
-    }
+    // {
+    //   username:"tuyenphk",
+    //   url: "https://i.ibb.co/YNyzWWy/cover.jpg",
+    //   caption: "Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
+    // },
+    // {
+    //   username:"tuyenphk",
+    //   url: "https://i.ibb.co/Tws3HWy/Capture.png",
+    //   caption: "You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
+    // },
+    // {
+    //   username:"tuyenphk",
+    //   url: "https://i.ibb.co/SxXVr7Q/1.png",
+    //   caption: "Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
+    // }
   ]);
+
+  useEffect (() => {
+    db.collection('posts')
+      .onSnapshot(snapshot => {
+        //every time a new post is added, this code launches to firestore
+        setPosts(snapshot.docs.map(doc => ({
+          id: doc.id,
+          post: doc.data()
+        })));
+      })
+  }, [])
 
   return (
     <div className="App">
@@ -30,8 +43,9 @@ function App() {
       </div>
 
       {
-        posts.map (post => (
-          <Post username={post.username}
+        posts.map (({id, post}) => (
+          <Post key={id}
+                username={post.username}
                 url={post.url}
                 caption={post.caption} />
         ))
