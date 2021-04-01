f5b621d816a78a3ac7ea578b4c8da9e7b769cdfa
Tuyen Pham <tuh39709@temple.edu>
2021-04-01 15:18:49 -0400

    add posts

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -10,11 +10,24 @@ function App() {
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
             alt="" />
       </div>
+
       <Post
           username="tuyenphk"
           url="https://i.ibb.co/YNyzWWy/cover.jpg"
           caption="Best scenery moment #canolafields #summervibes #usa #pennsylvania #lehighvalley #northampton #cherryville #covid19 👣👣🌾🌼🥰"
-           />
+      />
+
+      <Post
+          username="tuyenphk"
+          url="https://i.ibb.co/Tws3HWy/Capture.png"
+          caption="You makes me feel warm, snowman ☃️❄️❄️ #usa #philly #snowday #springbreak #comming 👣😘😂😚"
+      />
+
+      <Post
+          username="tuyenphk"
+          url="https://i.ibb.co/SxXVr7Q/1.png"
+          caption="Happy birthday Mom! Love you to the moon.. ❤️❤️❤️❤️ #April2nd2021"
+      />
     </div>
   );
 }
